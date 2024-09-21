4936d663d907eb7a6d37097f482df23753c64ef2
ray <rizkyray01@gmail.com>
2024-09-21 20:31:52 +0800

    add: database and vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
       ],
+
+
   theme: {
     extend: {},
   },
